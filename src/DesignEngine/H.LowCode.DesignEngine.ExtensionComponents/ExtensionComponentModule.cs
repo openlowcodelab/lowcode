abaebfff4dd3318385778924da0ee29b7e24e4bd
{iliuhai@aliyun.com} <iliuhai@aliyun.com>
2024-05-27 01:06:52 +0800

    实现 monaco-editor 组件

--- src/DesignEngine/H.LowCode.DesignEngine.ExtensionComponents/ExtensionComponentModule.cs
+++ src/DesignEngine/H.LowCode.DesignEngine.ExtensionComponents/ExtensionComponentModule.cs
@@ -5,10 +5,11 @@ using Volo.Abp.Modularity;
 
 namespace H.LowCode.DesignEngine.ExtensionComponents
 {
-    public class CustomComponentModule : AbpModule
+    public class ExtensionComponentModule : AbpModule
     {
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
+            context.Services.AddScoped<IComponentProvider, ExtensionComponentProvider>();
             context.Services.AddScoped<IPageTemplateProvider, ExtensionPageTemplateProvider>();
         }
     }
