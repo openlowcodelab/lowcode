29bcc14550256de51e3e37d74142836bf72e6e93
{iliuhai@aliyun.com} <iliuhai@aliyun.com>
2025-12-06 00:10:01 +0800

    修复列表组件渲染模式切换时数据加载问题

--- src/Common/H.LowCode.ComponentBase/CascadingModels/PageCascadingModel.cs
+++ src/Common/H.LowCode.ComponentBase/CascadingModels/PageCascadingModel.cs
@@ -5,11 +5,11 @@ namespace H.LowCode.ComponentBase;
 
 public class PageCascadingModel
 {
-    public string AppId { get; set; }
+    public required string AppId { get; set; }
 
-    public string PageId { get; set; }
+    public required string PageId { get; set; }
 
-    public string PageName { get; set; }
+    public required string PageName { get; set; }
 
     /// <summary>
     /// 页面布局（1:一列、2:二列、3:三列、4:四列）
