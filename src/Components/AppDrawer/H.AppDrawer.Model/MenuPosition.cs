cbdae8f7b4914caf06d9e5ff9f0e2a147836e9fc
{iliuhai@aliyun.com} <iliuhai@aliyun.com>
2026-05-11 01:09:04 +0800

    导航菜单支持顶部菜单，简化 WorkbenchNavLayout 逻辑

---
+++ src/Components/AppDrawer/H.AppDrawer.Model/MenuPosition.cs
@@ -0,0 +1,17 @@
+namespace H.AppDrawer.Components;
+
+/// <summary>
+/// 菜单位置枚举
+/// </summary>
+public enum MenuPosition
+{
+    /// <summary>
+    /// 左侧菜单
+    /// </summary>
+    Left,
+    
+    /// <summary>
+    /// 顶部菜单
+    /// </summary>
+    Top
+}
